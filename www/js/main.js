b82255aa9813141496f56249568120c892653338
logon2ashwin <ashwin.95b@gmail.com>
2018-03-25 21:09:41 +0530

    initial commit

---
+++ www/js/main.js
@@ -0,0 +1,60 @@
+
+var app = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        // this.receivedEvent('deviceready');
+    },
+    
+};
+
+app.initialize();
+
+var onlogin  = function(){
+    console.log('login');
+    window.alert('cannot connect to server');
+}
+
+var fingerprint = function(){
+    FingerprintAuth.isAvailable(function (result) {
+        
+        console.log("FingerprintAuth available: " + JSON.stringify(result));
+        // If has fingerprint device and has fingerprints registered
+        if (result.isAvailable == true && result.hasEnrolledFingerprints == true) {
+            var encryptConfig = {
+                clientId: "DSMT",
+                username: "raghul",
+                password: "raghul333",
+                maxAttempts: 5,
+                locale: "en_US",
+                dialogTitle: "Hey dude, your finger",
+                dialogMessage: "Put your finger on the device",
+                dialogHint: ""
+            };
+            alert(encryptConfig.username,encryptConfig.password);
+            // Set config and success callback
+            FingerprintAuth.encrypt(encryptConfig, function(_fingerResult){
+                // alert('detected');
+                console.log("successCallback(): " + JSON.stringify(_fingerResult));
+                if (_fingerResult.withFingerprint) {
+                    console.log("Successfully encrypted credentials.");
+                    console.log("Encrypted credentials: " + result.token);  
+                } else if (_fingerResult.withBackup) {
+                    console.log("Authenticated with backup password");
+                }
+            // Error callback
+            }, function(err){
+                    if (err === "Cancelled") {
+                    console.log("FingerprintAuth Dialog Cancelled!");
+                } else {
+                    console.log("FingerprintAuth Error: " + err);
+                }
+            });
+        }
+    }, function (message) {
+        console.log("isAvailableError(): " + message);
+        alert('No fingerprint sensor detected')
+    });
+}
